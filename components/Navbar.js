e801d785823ce9b92cb0d9eca669db8d12c0e2a9
future-coder01 <Jakekimblink5>
2026-06-21 17:53:26 +0100

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,14 @@
+     "use client";
+import { useState } from "react";     
 import Link from "next/link";
 
-export default function Navbar() {
+
+  export default function Navbar(){
+    const [dropDown,setDropDown] = useState(false);
+
+    const toggleMenu = ()=>{
+        setDropDown(!dropDown);
+    }
   
   return (
     <nav className="fixed left-0 top-0 z-50 w-full border-b border-white/10 bg-blue-600/95 backdrop-blur-md shadow-xl">
@@ -18,53 +26,29 @@ export default function Navbar() {
           </p>
         </div>
 
-        {/* Navigation Links */}
-        <ul className="hidden items-center gap-6 text-base font-semibold md:flex lg:gap-10 lg:text-lg">
+        {dropDown && (
+  <div className="md:hidden bg-blue-700 px-5 py-4 space-y-4 text-white font-semibold">
 
-          {/* Home */}
-          <li>
-            <Link
-              href="/"
-              className="relative text-white transition hover:text-yellow-300"
-            >
-              Home
-            </Link>
-          </li>
+    <Link href="/" onClick={() => setDropDown(false)}>
+      Home
+    </Link>
 
-          {/* Computer */}
-          <li>
-            <Link
-              href="/dashboard/computer-accessories"
-              className="relative text-white transition hover:text-yellow-300"
-            >
-              Computer-Accessories
-            </Link>
-          </li>
+    <Link href="/dashboard/computer-accessories" onClick={() => setDropDown(false)}>
+      Computer-Accessories
+    </Link>
 
-          {/* Solar */}
-          <li>
-            <Link
-              href="/dashboard/solar-item"
-              className="relative text-white transition hover:text-yellow-300"
-            >
-              Solar-Items
-            </Link>
-          </li>
+    <Link href="/dashboard/solar-item" onClick={() => setDropDown(false)}>
+      Solar-Items
+    </Link>
 
-          {/* Location */}
-          <li>
-            <Link
-              href="/dashboard/location"
-              className="relative text-white transition hover:text-yellow-300"
-            >
-              Location
-            </Link>
-          </li>
+    <Link href="/dashboard/location" onClick={() => setDropDown(false)}>
+      Location
+    </Link>
 
-        </ul>
-
+  </div>
+)}
         {/* Mobile Menu */}
-        <button className="text-white md:hidden">
+        <button className="text-white md:hidden" onClick={toggleMenu}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
